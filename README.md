7a98fa415a79cc34c341ac0eb67e994d74bce185
Marcos Velasco <devslife7@gmail.com>
2022-05-26 13:19:13 -0400

    added Readme text

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WebPTCodeChallenge
+
+## The Copier class: a class that copies, gets and tries to delete the given argument.
+
+// copy() method return the original argument
+// copy() method return a copy of the original argument if it is an Object
+// get() method returns a Promise with the original argument after 1 second
+// delete() method returns a Promise with an Error object after 2 seconds
--- copy.js
+++ copy.js
@@ -28,21 +28,25 @@ class Copier {
   }
 }
 
+// copy() method return the original argument
 const src = 'a'
 const copier = new Copier(src)
 const dest = copier.copy()
 console.log('dest: ', dest)
 
+// copy() method return a copy of the original argument if it is an Object
 const src1 = { a: 'b', c: { d: 'e', f: ['g', { h: 'i' }] } }
 const copier1 = new Copier(src1)
 const dest1 = copier1.copy()
 console.log('dest1: ', dest1)
 console.log('same object: ', dest1 === src1)
 
+// get() method returns a Promise with the original argument after 1 second
 const src2 = 'b'
 const copier2 = new Copier(src2)
 copier2.get().then(resp => console.log('dest2: ', resp))
 
+// delete() method returns a Promise with an Error object after 2 seconds
 const src3 = 'c'
 const copier3 = new Copier(src3)
 copier3.delete().then(resp => console.log('dest3: ', resp))
